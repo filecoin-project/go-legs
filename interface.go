982620010377eead7626db48dc1596ceef096e3f
gammazero <gammazero@users.noreply.github.com>
2021-12-17 15:40:17 -0800

    remove unused interfaces

--- interface.go
+++ interface.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/ipfs/go-cid"
 	"github.com/ipld/go-ipld-prime"
-	"github.com/libp2p/go-libp2p-core/peer"
-	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
@@ -18,44 +16,6 @@ type Publisher interface {
 	UpdateRootWithAddrs(context.Context, cid.Cid, []ma.Multiaddr) error
 	// Close publisher
 	Close() error
-}
-
-// LegSubscriber is an interface for watching a published dag.
-type LegSubscriber interface {
-	// OnChange returns a listener and cancel func for subscriber.  When cancel
-	// is called the listener channel is closed.
-	OnChange() (chan cid.Cid, context.CancelFunc)
-	// SetPolicyHandler triggered to know if an exchange needs to be made.
-	SetPolicyHandler(PolicyHandler) error
-	// SetLatestSync updates the latest sync of a subcriber to account for
-	// out of band updates.
-	SetLatestSync(c cid.Cid) error
-	// Sync to a specific Cid of a peer's DAG without having to wait for a
-	// publication.
-	// Returns a channel that will resolve with the same cid, c that was passed as input when
-	// the dag from 'c' is available, a function to cancel, or a synchronous error if
-	// the subscriber is not in a state where it can sync from the specified peer.
-	Sync(ctx context.Context, p peer.ID, c cid.Cid, selector ipld.Node) (<-chan cid.Cid, context.CancelFunc, error)
-	// Close subscriber
-	Close() error
-	// LatestSync gets the latest synced link.
-	LatestSync() ipld.Link
-}
-
-// PolicyHandler make some preliminary checks before running the exchange
-type PolicyHandler func(*pubsub.Message) (bool, error)
-
-// FilterPeerPolicy is a sample policy that only triggers exchanges
-// if the update is generated from a specific peer.
-func FilterPeerPolicy(p peer.ID) PolicyHandler {
-	return func(msg *pubsub.Message) (bool, error) {
-		from := msg.GetFrom()
-		allow := from == p
-		if !allow {
-			log.Debugf("Filtered pubsub message from %s", from)
-		}
-		return allow, nil
-	}
 }
 
 // Syncer is the interface used to sync with a data source.
