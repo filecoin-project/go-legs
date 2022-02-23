45a7a691d129683551b9e864059d79d89998fbfb
Andrew Gillis <gammazero@users.noreply.github.com>
2022-02-23 02:04:29 -0800

    Add CheckAlreadySynced sync option (#94)
    * Add CheckAlreadySynced sync option
    
    This is necessary to check if already synced to the current head, when a custom selector is passed in.  If the selector has a stop node, the selector only compares the previous link against the stop node.  So, if the current head is the stop node, this needs a special case check.  Normally, when a custom selector is passed in this special case check is not done (since legs does not know what the selector does).  This option lets the caller tell legs to do the special case check.
    
    * bump version

--- option.go
+++ option.go
@@ -87,20 +87,31 @@ func SyncRecursionLimit(limit selector.RecursionLimit) Option {
 }
 
 type syncCfg struct {
-	scopedBlockHook    BlockHookFunc
 	alwaysUpdateLatest bool
+	checkAlreadySynced bool
+	scopedBlockHook    BlockHookFunc
 }
 
 type SyncOption func(*syncCfg)
 
-func ScopedBlockHook(hook BlockHookFunc) SyncOption {
-	return func(sc *syncCfg) {
-		sc.scopedBlockHook = hook
-	}
-}
-
 func AlwaysUpdateLatest() SyncOption {
 	return func(sc *syncCfg) {
 		sc.alwaysUpdateLatest = true
+	}
+}
+
+// CheckAlreadySynced is used when a custom selector is provided, and checks if
+// the head node returned from the publisher is the latest seen.  This is
+// needed because a selector with a stop node only recognizes if the previous
+// link is the stop, not if the current node is.
+func CheckAlreadySynced() SyncOption {
+	return func(sc *syncCfg) {
+		sc.checkAlreadySynced = true
+	}
+}
+
+func ScopedBlockHook(hook BlockHookFunc) SyncOption {
+	return func(sc *syncCfg) {
+		sc.scopedBlockHook = hook
 	}
 }
