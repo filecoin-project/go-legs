3da8953eb2897ca36127e61331bfb92a1b6313ba
Andrew Gillis <gammazero@users.noreply.github.com>
2022-05-17 02:18:23 -0700

    Fix rate limit burst refresh for graphsync (#124)
    * Fix rate limit burst refresh for graphsync
    
    After dt/gs session hits the burst limit, it is necessary to wait for the token bucket to refill before restarting the session. Previously, the wait was happening but was also emptying its bucket.  So when the session restarted, the token bucket was empty and rate-limiting happened immediately when reading the next block.  This fixes this to wait without emptying the bucket.
    
    Other changes:
    - Improved logging
    - AllowPeerFunc only returns bool. It is up to the function to log any errors, not legs.  Also returning the error does not allow any decision beyond what the boolean return value already does.

--- dtsync/syncer.go
+++ dtsync/syncer.go
@@ -3,6 +3,7 @@ package dtsync
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/filecoin-project/go-legs/p2p/protocol/head"
 	"github.com/ipfs/go-cid"
@@ -58,14 +59,28 @@ func (s *Syncer) Sync(ctx context.Context, nextCid cid.Cid, sel ipld.Node) error
 		// Wait for transfer finished signal.
 		err = <-syncDone
 		if err, ok := err.(rateLimitErr); ok {
-			log.Infow("hit rate limit. Waiting and will retry later", "cid", nextCid, "source_peer", s.peerID)
-
-			// Wait until we've fully refilled our rate limit bucket since this is a
-			// relatively heavy operation (essentially restarting the sync).
-			waitErr := s.rateLimiter.WaitN(ctx, s.rateLimiter.Burst())
-			if waitErr != nil {
-				return err
+			// Wait until the rate limit bucket is fully refilled since this is
+			// a relatively heavy operation (essentially restarting the sync).
+			// Note, cannot use s.rateLimiter.WaitN here because that waits,
+			// but also consumes n tokens.
+			waitMsec := 1000.0 * float64(s.rateLimiter.Burst()) / float64(s.rateLimiter.Limit())
+			var waitTime time.Duration
+			waitTime = time.Duration(waitMsec) * time.Millisecond
+			if waitTime == 0 {
+				waitTime = time.Duration(1000*waitMsec) * time.Microsecond
 			}
+			log.Infow("Hit rate limit. Waiting and will retry later", "cid", nextCid, "source_peer", s.peerID, "delay", waitTime.String())
+			select {
+			case <-time.After(waitTime):
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+			// Need to consume one token, since the stopped to make up for the
+			// previous Allow that did not consume a token and triggered rate
+			// limiting, even though the block was still downloaded. At next
+			// restart the stopped at block will be local and will not count
+			// toward rate limiting.
+			s.rateLimiter.Allow()
 
 			// Set the nextCid to be the cid that we stopped at becasuse of rate
 			// limitting. This lets us pick up where we left off
