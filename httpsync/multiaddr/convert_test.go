87181395322196bafa744806da1b4271bfc33caf
Andrew Gillis <gammazero@users.noreply.github.com>
2022-07-12 08:25:25 -0700

    Convert multiaddr to URL should handle /tls (#141)
    * Convert multiaddr to URL should handle /tls
    
    When converting a multiaddr to a URL, "/https" and "/tls/http" should both convert to "https://". Also handle "/wss" and "/tls/ws" similarly.
    
    - Fix: When converting url to multiaddr, return `multiaddr.Multiaddr` interface, not pointer to interface.

--- httpsync/multiaddr/convert_test.go
+++ httpsync/multiaddr/convert_test.go
@@ -3,6 +3,8 @@ package multiaddr
 import (
 	"net/url"
 	"testing"
+
+	"github.com/multiformats/go-multiaddr"
 )
 
 func TestRoundtrip(t *testing.T) {
@@ -16,11 +18,11 @@ func TestRoundtrip(t *testing.T) {
 
 	for _, s := range samples {
 		u, _ := url.Parse(s)
-		mu, err := ToMA(u)
+		mu, err := ToMultiaddr(u)
 		if err != nil {
 			t.Fatal(err)
 		}
-		u2, err := ToURL(*mu)
+		u2, err := ToURL(mu)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -32,6 +34,41 @@ func TestRoundtrip(t *testing.T) {
 		}
 		if u2.Path != u.Path {
 			t.Fatalf("path didn't roundtrip. got %s, expected %s", u2.Path, u.Path)
+		}
+	}
+}
+
+func TestTLSProtos(t *testing.T) {
+	samples := []string{
+		"/ip4/192.169.0.1/tls/http",
+		"/ip4/192.169.0.1/https",
+		"/ip4/192.169.0.1/http",
+		"/dns4/protocol.ai/tls/ws",
+		"/dns4/protocol.ai/wss",
+		"/dns4/protocol.ai/ws",
+	}
+
+	expect := []string{
+		"https://192.169.0.1",
+		"https://192.169.0.1",
+		"http://192.169.0.1",
+		"wss://protocol.ai",
+		"wss://protocol.ai",
+		"ws://protocol.ai",
+	}
+
+	for i := range samples {
+		m, err := multiaddr.NewMultiaddr(samples[i])
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		u, err := ToURL(m)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if u.String() != expect[i] {
+			t.Fatalf("expected %s to convert to url %s, got %s", m.String(), expect[i], u.String())
 		}
 	}
 }
